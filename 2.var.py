fdb30bae7eccf03356771d400538755772b8798e
prokopii <pizzadestroy@gmail.com>
2023-02-16 23:33:11 +0900

    added 2.var.py

---
+++ 2.var.py
@@ -0,0 +1,55 @@
+# 
+# asd
+# asd
+# asd
+
+'''
+dsfsdf
+sdfsdf
+sdfsdf
+gfdghdfhdf
+dfhhgggf
+
+
+'''
+# Документация PEP8 - рекомендуется для чтения
+# Переменные
+
+# snake_case 
+var_1 = 100
+
+# camelCase
+myNumVar = 50
+
+# константные переменные (константы)
+CONST_VAR = 1000
+
+# CONST_VAR = 50
+
+# вывод значения переменной в терминал
+# print(var_1 + CONST_VAR)
+
+# встроенная функция для ввода данных из терминала 
+# in_1 = input("Введите значение: ")
+
+# print(in_1)
+
+# in_1 = input("Введите ваше имя: ")
+
+# print("Привет, ", in_1,"!")
+
+# a = input("asd ")
+# b = input("sdf ")
+# print(a + b)
+
+a = input("asd ")
+b = input("sdf ")
+
+# конкатенация (объединение) строк 
+# c = a + b
+
+# математическое суммирование целых чисел
+
+c = int(a) + int(b)
+
+print(c)
